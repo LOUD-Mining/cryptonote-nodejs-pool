70c7800e8033b7da812224d03225f92c2aa667b8
LOUD Mining <39448763+LOUD-Mining@users.noreply.github.com>
2020-06-30 03:52:50 -0500

    Update config.js

--- website/pools/trtl/config.js
+++ website/pools/trtl/config.js
@@ -1,9 +1,9 @@
-var api = "https://funwithcrypto.tk:8125";
+var api = "https://loudmining.com/trtl/api";
 let parentCoin = "turtlecoin";
 
 //var email = "";
-var telegram = "https://t.me/FunWithCrypto";
-var discord = "https://discord.gg/nRPWrTB";
+//var telegram = "";
+var discord = "https://discord.gg/rvTZV5F";
 //var facebook = "";
 
 var marketCurrencies = ["{symbol}-BTC", "{symbol}-LTC", "{symbol}-USD", "{symbol}-EUR", "{symbol}-CAD"];
